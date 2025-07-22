3de4a666fa7b70e717aabd1683720971f44484dd
khalil <khalilulrehman2029@gmail.com>
2025-07-22 11:01:57 -0700

    Initial commit for extension

---
+++ content_script_main_site.js
@@ -0,0 +1,31 @@
+console.log("Script injected")
+
+async function updateNumbeStatus(number) {
+    // Update status in table for numbers which have no status 
+    var elems = document.querySelectorAll("td[data-type='status']")
+    for (var i = 0; i < elems.length; i++) {
+        elem = elems[i];
+        if (!(elem.innerHTML.toString().trim())) {
+            // No status added right now add one
+            numberInElem = elem.getAttribute("data").toString().trim()
+            if (numberInElem === number) {
+                elem.innerHTML = "<img src='https://raw.githubusercontent.com/DefCon-007/WhatSend/master/images/Check.svg?sanitize=true' />"
+                elem.setAttribute("data-sort", "success");
+            } else {
+                elem.innerHTML = "<img src='https://raw.githubusercontent.com/DefCon-007/WhatSend/master/images/False.svg?sanitize=true' />"
+            }
+        }
+    }
+
+}
+
+// Listen for messages from background script to update number status
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log("Message received");
+        if (request.type === "UPDATE_STATE") {
+            // Message was successfully sent 
+            console.log("Updating number status")
+            updateNumbeStatus(request.number);
+        }
+    });
---
+++ popup.js
@@ -0,0 +1,39 @@
+
+let browserApi;
+try {
+    browserApi = browser;
+} catch (e) {
+    if (e instanceof ReferenceError) {
+        console.log('switch to chrome api');
+        browserApi = chrome;
+    } else {
+        throw (e);
+    }
+}
+
+
+function setCheckedState(state) {
+    var elem = document.getElementById('onoffswitch');
+    if (state.shouldAutoSend === undefined){
+        browserApi.storage.local.set({ shouldAutoSend: true });
+        elem.checked = true
+    } else {
+        elem.checked = state.shouldAutoSend;
+    }
+    
+}
+browserApi.storage.local.get(['shouldAutoSend'], setCheckedState)
+
+// Setup the checkbox
+var elem = document.getElementById('onoffswitch');
+
+elem.addEventListener('change', (event) => {
+    // Checkbox value changed!!!
+    console.log("settigns")
+    browserApi.storage.local.set({ shouldAutoSend: event.target.checked })
+})
+
+var button = document.getElementById("websiteButton");
+button.addEventListener("click", function(){
+    browserApi.tabs.create({url:"https://whatsend.defcon007.com/"});
+});
